9b6cb50aebb11d27fbac1a43d3f864acd6893b71
undefinedSynthesis <undefinedsynthesis@gmail.com>
2023-02-04 20:50:30 +0900

    アロー関数を使うべきでない例を追加

--- reduce.js
+++ reduce.js
@@ -22,7 +22,20 @@ Profiles.prototype.consoleTable = function () {
     console.table(this.band);
 }
 
+// アロー関数を使うべきでない例
+// ここでのthisは呼び出し元のオブジェクトではない
+// Profiles.prototype.sum = () => {
+//     return this.band.reduce((prev, curr) => prev + curr.age, 0);
+// }
+
+// functionを使って宣言するとthisは呼び出し元のオブジェクトを指す
+Profiles.prototype.sum = function() {
+    return this.band.reduce((prev, curr) => prev + curr.age, 0);
+}
+
 const profile = new Profiles(kessokuBand);
 console.log(profile.calcMaxAge());
 console.log(profile.calcMinAge());
 profile.consoleTable();
+
+console.log(profile.sum());
